e6e97214b984ff4e18569bc8176345a579a5647c
Nathan Lei <woooms@qq.com>
2025-02-10 16:12:56 +0800

    feat: add novel editor and strapi integration

--- apps/web/app/api/generate/route.ts
+++ apps/web/app/api/generate/route.ts
@@ -1,8 +1,7 @@
-import { openai } from "@ai-sdk/openai";
 import { Ratelimit } from "@upstash/ratelimit";
 import { kv } from "@vercel/kv";
-import { streamText } from "ai";
 import { match } from "ts-pattern";
+import { NextResponse } from "next/server";
 
 // IMPORTANT! Set the runtime to edge: https://vercel.com/docs/functions/edge-functions/edge-runtime
 export const runtime = "edge";
@@ -10,9 +9,12 @@ export const runtime = "edge";
 export async function POST(req: Request): Promise<Response> {
   // Check if the OPENAI_API_KEY is set, if not return 400
   if (!process.env.OPENAI_API_KEY || process.env.OPENAI_API_KEY === "") {
-    return new Response("Missing OPENAI_API_KEY - make sure to add it to your .env file.", {
-      status: 400,
-    });
+    return new Response(
+      "Missing OPENAI_API_KEY - make sure to add it to your .env file.",
+      {
+        status: 400,
+      },
+    );
   }
   if (process.env.KV_REST_API_URL && process.env.KV_REST_API_TOKEN) {
     const ip = req.headers.get("x-forwarded-for");
@@ -21,7 +23,9 @@ export async function POST(req: Request): Promise<Response> {
       limiter: Ratelimit.slidingWindow(50, "1 d"),
     });
 
-    const { success, limit, reset, remaining } = await ratelimit.limit(`novel_ratelimit_${ip}`);
+    const { success, limit, reset, remaining } = await ratelimit.limit(
+      `novel_ratelimit_${ip}`,
+    );
 
     if (!success) {
       return new Response("You have reached your request limit for the day.", {
@@ -68,7 +72,8 @@ export async function POST(req: Request): Promise<Response> {
       {
         role: "system",
         content:
-          "You are an AI writing assistant that shortens existing text. " + "Use Markdown formatting when appropriate.",
+          "You are an AI writing assistant that shortens existing text. " +
+          "Use Markdown formatting when appropriate.",
       },
       {
         role: "user",
@@ -115,15 +120,7 @@ export async function POST(req: Request): Promise<Response> {
     ])
     .run();
 
-  const result = await streamText({
-    prompt: messages[messages.length - 1].content,
-    maxTokens: 4096,
-    temperature: 0.7,
-    topP: 1,
-    frequencyPenalty: 0,
-    presencePenalty: 0,
-    model: openai("gpt-4o-mini"),
+  return NextResponse.json({
+    messages: messages,
   });
-
-  return result.toDataStreamResponse();
 }
